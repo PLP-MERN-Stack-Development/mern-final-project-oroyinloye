05ff5933e660213cfc8f3f37d8b58027c4f1f21e
Richard Oyinloye <richard.oyinloye@gmail.com>
2025-11-22 14:35:26 +0100

    Initial commit with working backend and frontend

--- agroconnect-backend/server.js
+++ agroconnect-backend/server.js
@@ -18,6 +18,9 @@ mongoose.connect(process.env.MONGO_URI)
 
 // use routes
 app.use("/api/auth", authRoutes);
+app.get("/", (req, res) => {
+  res.send("AgroConnect Backend is running ✅");
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
